bee4e75a1cdadb09b7d6aaa64fcbaad7c732a6a7
Byron Whitlock <byronwhitlock@google.com>
2024-12-03 13:24:23 -0800

    QOL improvments for command line parsing, extra docs, cleanup unused variables

--- main.go
+++ main.go
@@ -1,42 +1,41 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	rawLog "log"
-	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/lqqyt2423/go-mitmproxy/addon"
-	//"github.com/lqqyt2423/go-mitmproxy/internal/helper"
 	"github.com/lqqyt2423/go-mitmproxy/proxy"
 	"github.com/lqqyt2423/go-mitmproxy/web"
 	log "github.com/sirupsen/logrus"
 )
 
 // makefile will turn this into a version
-var Version = "undefined"
+var Version = ".3"
 
 type Config struct {
 	version bool // show version
 
-	Addr        string   // proxy listen addr
-	WebAddr     string   // web interface listen addr
-	SslInsecure bool     // not verify upstream server SSL/TLS certificates.
-	IgnoreHosts []string // a list of ignore hosts
-	AllowHosts  []string // a list of allow hosts
-	CertPath    string   // path of generate cert files
-	Debug       int      // debug mode: 1 - print debug log, 2 - show debug from
-	Dump        string   // dump filename
-	DumpLevel   int      // dump level: 0 - header, 1 - header + body
+	Addr        string // proxy listen addr
+	WebAddr     string // web interface listen addr
+	SslInsecure bool   // not verify upstream server SSL/TLS certificates.
+
+	CertPath string // path of generate cert files
+	Debug    int    // debug mode: 1 - print debug log, 2 - show debug from
+
+	Dump      string // dump filename
+	DumpLevel int    // dump level: 0 - header, 1 - header + body
 
 	// kms options
 	KmsResourceName string
 
 	Upstream     string // upstream proxy
 	UpstreamCert bool   // Connect to upstream server to look up certificate details. Default: True
-
-	KmsURI string // URI to KMS key for encryption
 }
 
 // global config variable
@@ -44,6 +43,11 @@ var config *Config
 
 func main() {
 	config = loadConfig()
+	if config.version {
+		fmt.Println("go-gcsproxy: " + Version)
+		Usage()
+		os.Exit(0)
+	}
 
 	if config.Debug > 0 {
 		rawLog.SetFlags(rawLog.LstdFlags | rawLog.Lshortfile)
@@ -59,6 +63,18 @@ func main() {
 		FullTimestamp: true,
 	})
 
+	if config.KmsResourceName == "" {
+		fmt.Printf("\n>>> kms_resource_name empty.\n")
+		Usage()
+		os.Exit(0)
+	}
+
+	err := CheckKMS()
+	if err != nil {
+		fmt.Printf("\n>>> unable to initialize Google KMS. %v", err)
+		os.Exit(0)
+	}
+
 	opts := &proxy.Options{
 		Debug:             config.Debug,
 		Addr:              config.Addr,
@@ -71,25 +87,6 @@ func main() {
 	p, err := proxy.NewProxy(opts)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if config.version {
-		fmt.Println("go-gcsproxy: " + Version)
-		os.Exit(0)
-	}
-
-	log.Infof("go-gcsproxy version %v\n", Version)
-
-	if len(config.IgnoreHosts) > 0 {
-		p.SetShouldInterceptRule(func(req *http.Request) bool {
-			return false
-			//return !helper.MatchHost(req.Host, config.IgnoreHosts)
-		})
-	}
-	if len(config.AllowHosts) > 0 {
-		p.SetShouldInterceptRule(func(req *http.Request) bool {
-			return true // helper.MatchHost(req.Host, config.AllowHosts)
-		})
 	}
 
 	if !config.UpstreamCert {
@@ -108,6 +105,10 @@ func main() {
 		p.AddAddon(dumper)
 	}
 
+	configJson, _ := json.MarshalIndent(config, "", "\t")
+	msg := fmt.Sprintf("go-gcsproxy version '%v' Started. %v", config.version, string(configJson))
+	log.Info(msg)
+
 	log.Fatal(p.Start())
 }
 
@@ -117,39 +118,56 @@ func loadConfig() *Config {
 	defaultSslInsecure := true
 	defaultCertPath := "/proxy/certs"
 	defaultDebug := 0
-	defaultKmsResourceName := "projects/ymail-central-logsink-0357/locations/global/keyRings/gcsproxy-test/cryptoKeys/gcsproxy-test-ring"
+	defaultKmsResourceName := ""
 
-	setBoolEnvVar("SSL_INSECURE", &defaultSslInsecure)
-	setStringEnvVar("PROXY_CERT_PATH", &defaultCertPath)
-	setIntEnvVar("DEBUG_LEVEL", &defaultDebug)
-	setStringEnvVar("GCP_KMS_RESOURCE_NAME", &defaultKmsResourceName)
+	sslInsecure, sslInsecureErr := strconv.ParseBool("SSL_INSECURE")
+	if sslInsecureErr == nil {
+		defaultSslInsecure = sslInsecure
+	}
 
+	certPath := os.Getenv("PROXY_CERT_PATH")
+	if certPath != "" {
+		defaultCertPath = certPath
+	}
+
+	debug, debugErr := strconv.Atoi(os.Getenv("DEBUG_LEVEL"))
+	if debugErr == nil {
+		defaultDebug = debug
+	}
+
+	kmsResourceName := os.Getenv("GCP_KMS_RESOURCE_NAME")
+	if kmsResourceName != "" {
+		defaultKmsResourceName = kmsResourceName
+	}
 	flag.BoolVar(&config.version, "version", false, "show go-gcsproxy version")
-	flag.StringVar(&config.Addr, "addr", ":9080", "proxy listen addr")
-	flag.StringVar(&config.WebAddr, "web_addr", ":9081", "web interface listen addr")
-	flag.BoolVar(&config.SslInsecure, "ssl_insecure", defaultSslInsecure, "not verify upstream server SSL/TLS certificates.")
-	flag.Var((*arrayValue)(&config.IgnoreHosts), "ignore_hosts", "a list of ignore hosts")
-	flag.Var((*arrayValue)(&config.AllowHosts), "allow_hosts", "a list of allow hosts")
-	flag.StringVar(&config.CertPath, "cert_path", defaultCertPath, "path to generated cert files")
-	flag.IntVar(&config.Debug, "debug", defaultDebug, "debug mode: 1 - print debug log, 2 - show debug from")
-	flag.StringVar(&config.Dump, "dump", "", "dump filename")
+	flag.StringVar(&config.Addr, "port", ":9080", "proxy listen addr")
+	flag.StringVar(&config.WebAddr, "web_port", ":9081", "web interface listen addr")
+	flag.BoolVar(&config.SslInsecure, "ssl_insecure", defaultSslInsecure, "don't verify upstream server SSL/TLS certificates.")
+
+	flag.StringVar(&config.CertPath, "cert_path", defaultCertPath, "path to cert. if 'mitmproxy-ca.pem' is not present here, it will be generated.")
+	flag.IntVar(&config.Debug, "debug", defaultDebug, "debug level: 0 - ERROR, 1 - DEBUG, 2 - TRACE")
+	flag.StringVar(&config.Dump, "dump", "", "filename to dump req/responses for debugging")
 	flag.IntVar(&config.DumpLevel, "dump_level", 0, "dump level: 0 - header, 1 - header + body")
 	flag.StringVar(&config.Upstream, "upstream", "", "upstream proxy")
-	flag.StringVar(&config.KmsResourceName, "kms_project", defaultKmsResourceName, "Payload will be encrypted with keys stored in KMS ")
+	flag.StringVar(&config.KmsResourceName, "kms_resource_name", defaultKmsResourceName, "payload will be encrypted with this key stored in KMS. Must be in the format: projects/<project_id>/locations/<global|region>/keyRings/<key_ring>/cryptoKeys/<key>")
 
 	flag.BoolVar(&config.UpstreamCert, "upstream_cert", false, "connect to upstream server to look up certificate details")
 	flag.Parse()
 
 	return config
 }
-
-type arrayValue []string
-
-func (a *arrayValue) String() string {
-	return fmt.Sprint(*a)
+func Usage() {
+	flag.Usage()
+	fmt.Println("\nEnvironment variables supported:")
+	fmt.Println("  GCP_KMS_RESOURCE_NAME")
+	fmt.Println("  PROXY_CERT_PATH")
+	fmt.Println("  SSL_INSECURE")
+	fmt.Println("  DEBUG_LEVEL")
 }
 
-func (a *arrayValue) Set(value string) error {
-	*a = append(*a, value)
-	return nil
+func CheckKMS() error {
+	var ctx = context.TODO()
+
+	_, err := encryptBytes(ctx, config.KmsResourceName, []byte("Hello, World!"))
+	return err
 }
