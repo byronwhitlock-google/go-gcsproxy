820a632378d4838fd0c8083da5935590f8b98b4d
Lalitha Madhurima Gayathri Kolluru <lkolluru@google.com>
2025-04-01 09:17:38 -0700

    cache implemented for kms clients

--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/google/tink/go/aead"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -30,6 +31,7 @@ type Config struct {
 	// kms options
 	kmsBucketKeyMappingString string
 	KmsBucketKeyMapping       map[string]string
+	KMSEnvelopeAEADClientMapping       map[string]*aead.KMSEnvelopeAEAD
 
 	Upstream        string // upstream proxy
 	UpstreamCert    bool   // Connect to upstream server to look up certificate details. Default: True
@@ -64,6 +66,7 @@ func LoadConfig() *Config {
 	flag.BoolVar(&config.UpstreamCert, "upstream_cert", false, "connect to upstream server to look up certificate details")
 	flag.Parse()
 	config.KmsBucketKeyMapping = getBucketKeyMappings(config.kmsBucketKeyMappingString)
+	config.KMSEnvelopeAEADClientMapping = make(map[string]*aead.KMSEnvelopeAEAD)
 	config.GCSProxyVersion = "0.3"
 	GlobalConfig = config
 	return config
--- crypto/encrypt-decrypt.go
+++ crypto/encrypt-decrypt.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"time"
 
+	cfg "github.com/byronwhitlock-google/go-gcsproxy/config"
 	"github.com/google/tink/go/aead"
 	"github.com/google/tink/go/core/registry"
 	"github.com/google/tink/go/integration/gcpkms"
@@ -50,35 +51,64 @@ func Base64MD5Hash(byteStream []byte) string {
 	return base64MD5Hash
 }
 
+func GetKMSEnvelopeClient(resourceName string) (*aead.KMSEnvelopeAEAD, error){
+	clientMap := cfg.GlobalConfig.KMSEnvelopeAEADClientMapping
+	
+	if value, exists := clientMap[resourceName]; exists {
+		log.Debugf("KMS Envelope Client entry exists with value: %v", value)
+		return value,nil
+	}
+	return nil,fmt.Errorf( "KMS Envelope Client entry does not exist")
+}
+
+
+func getNewKMSEnvelopeClient(ctx context.Context, resourceName string) (*aead.KMSEnvelopeAEAD, error) {
+	// Construct the full key URI for Google Cloud KMS
+	envelopeClient, err := GetKMSEnvelopeClient(resourceName)
+	if err == nil {
+		return envelopeClient,nil
+	}else{
+		//projects/<projectname>/locations/<location>/keyRings/<project>/cryptoKeys/<key-ring>/cryptoKeyVersions/1
+		keyURI := fmt.Sprintf("gcp-kms://%s", resourceName)
+
+		// Create a KMS client
+		kmsClient, err := gcpkms.NewClientWithOptions(ctx, keyURI)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create KMS client: %v", err)
+		}
+
+		// Create a KMS AEAD client
+		kmsAEAD, err := kmsClient.GetAEAD(keyURI)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create KMS AEAD client: %v", err)
+		}
+
+		// 2. Register the KMS AEAD primitive wrapper.
+		registry.RegisterKMSClient(kmsClient)
+
+		// 3. Create the KMS-backed envelope AEAD.
+		envAEAD := aead.NewKMSEnvelopeAEAD2(aead.AES256GCMKeyTemplate(), kmsAEAD)
+		if envAEAD == nil {
+			return nil, fmt.Errorf("failed to create KMS AEAD envelope: %v", err)
+		}
+
+		cfg.GlobalConfig.KMSEnvelopeAEADClientMapping[resourceName] = envAEAD
+
+		return envAEAD, nil
+	}
+
+}	
+
 // Encrypt bytes with KMS key referenced by resourceName in the format:
 // projects/<projectname>/locations/<location>/keyRings/<project>/cryptoKeys/<key-ring>/cryptoKeyVersions/1
 func EncryptBytes(ctx context.Context, resourceName string, bytesToEncrypt []byte) ([]byte, error) {
 	// Capture the encryption latency
 	latencyStart := time.Now()
 
-	// Construct the full key URI for Google Cloud KMS
-	//projects/<projectname>/locations/<location>/keyRings/<project>/cryptoKeys/<key-ring>/cryptoKeyVersions/1
-	keyURI := fmt.Sprintf("gcp-kms://%s", resourceName)
-
-	// Create a KMS client
-	kmsClient, err := gcpkms.NewClientWithOptions(ctx, keyURI)
+	//Creating KMS Envelope client
+	envAEAD, err := getNewKMSEnvelopeClient(ctx, resourceName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create KMS client: %v", err)
-	}
-
-	// Create a KMS AEAD client
-	kmsAEAD, err := kmsClient.GetAEAD(keyURI)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create KMS AEAD client: %v", err)
-	}
-
-	// 2. Register the KMS AEAD primitive wrapper.
-	registry.RegisterKMSClient(kmsClient)
-
-	// 3. Create the KMS-backed envelope AEAD.
-	envAEAD := aead.NewKMSEnvelopeAEAD2(aead.AES256GCMKeyTemplate(), kmsAEAD)
-	if envAEAD == nil {
-		return nil, fmt.Errorf("failed to create KMS AEAD envelope: %v", err)
+		return nil, fmt.Errorf("error creating KMS envelope: %v", err)
 	}
 
 	// Encrypt the bytes
@@ -103,28 +133,11 @@ func EncryptBytes(ctx context.Context, resourceName string, bytesToEncrypt []byt
 func DecryptBytes(ctx context.Context, resourceName string, bytesToDecrypt []byte) ([]byte, error) {
 	// Capture the decryption latency
 	latencyStart := time.Now()
-	// Construct the full key URI for Google Cloud KMS
-	keyURI := fmt.Sprintf("gcp-kms://%s", resourceName)
 
-	// Create a KMS client
-	kmsClient, err := gcpkms.NewClientWithOptions(ctx, keyURI /*, option.WithCredentialsFile("path/to/credentials.json")*/)
+	//Creating KMS Envelope client
+	envAEAD, err := getNewKMSEnvelopeClient(ctx, resourceName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create KMS client: %v", err)
-	}
-
-	// Create a KMS AEAD client
-	kmsAEAD, err := kmsClient.GetAEAD(keyURI)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create KMS AEAD client: %v", err)
-	}
-
-	// Register the KMS AEAD primitive wrapper.
-	registry.RegisterKMSClient(kmsClient)
-
-	// Create the KMS-backed envelope AEAD.
-	envAEAD := aead.NewKMSEnvelopeAEAD2(aead.AES256GCMKeyTemplate(), kmsAEAD)
-	if envAEAD == nil {
-		return nil, fmt.Errorf("failed to create KMS AEAD envelope: %v", err)
+		return nil, fmt.Errorf("error creating KMS envelope: %v", err)
 	}
 	// Decrypt bytes with KMS key
 	aad := []byte("")
